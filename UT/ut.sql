b4bb3756d8e5c6853a3e762d4440b3c8157f905a
Shay Cohen <shayc91@gmail.com>
2021-09-15 09:54:53 +0300

    update ut

--- UT/ut.sql
+++ UT/ut.sql
@@ -4,21 +4,21 @@ SELECT [CalendarDate]
       ,[number_of_registrations]
       ,[number_of_purchases]
       ,[total_billing]
-  FROM [DW_Measurements].[dbo].[utm_report]
+  FROM [utm_report]
   where [total_billing] <> 0 
     or [number_of_purchases] <> 0 
 	or [number_of_registrations] <> 0
 
 
 SELECT TOP (1000) [CalendarDate]
-  FROM [DW_Measurements].[dbo].[dim_date]
+  FROM [dim_date]
 
 
 SELECT  min([CalendarDate])
-  FROM [DW_Measurements].[dbo].[utm_report]
+  FROM [utm_report]
 
 SELECT  max([CalendarDate])
-	FROM [DW_Measurements].[dbo].[utm_report]
+	FROM [utm_report]
 
  
 EXEC	[date_dim_builder]
